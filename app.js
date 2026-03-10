bfcfb5d855251df69cada1a82793182f318ebff1
CrazyKrabbyPatty <CrazyKrabbyPatty@gmail.com>
2026-03-10 17:51:02 +0300

    sequelize connection added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import sequelize from "./db.js";
 import pg from 'pg';
 
 let PORT = 5000;
@@ -11,20 +12,22 @@ app.use(express.json());
 // console.log(process.env)
 
 async function Start_App(){
-    let Client = new pg.Client({
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        host: process.env.PG_HOST,
-        port: process.env.PG_PORT,
-        database: process.env.PG_DATABASE,
-    })
+    // let Client = new pg.Client({
+    //     user: process.env.PG_USER,
+    //     password: process.env.PG_PASSWORD,
+    //     host: process.env.PG_HOST,
+    //     port: process.env.PG_PORT,
+    //     database: process.env.PG_DATABASE,
+    // })
 
     try{
-        await Client.connect();
+        // await Client.connect();
+        await sequelize.authenticate();
+        await sequelize.sync();
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch(error){
-        console.log("Error to bd connect " + error)
+        console.log("Error to start app connect: " + error)
     }
 }
 
-Start_App();
+Start_App().then(() => console.log("Приложение запущено успешно!"));
