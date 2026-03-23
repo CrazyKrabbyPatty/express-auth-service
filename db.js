e02e4f599f9f9af4fb0f5cc1705538f1187ca196
CrazyKrabbyPatty <CrazyKrabbyPatty@gmail.com>
2026-03-23 11:27:23 +0300

    docker files changes

--- db.js
+++ db.js
@@ -1,4 +1,7 @@
 import {Sequelize} from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const sequelize = new Sequelize(
     process.env.PG_DATABASE,
@@ -8,7 +11,14 @@ const sequelize = new Sequelize(
         dialect: 'postgres',
         host: process.env.PG_HOST,
         port: process.env.PG_PORT,
-    }
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        logging: false,
+    },
 )
 
 export default sequelize;
--- models/user-model.js
+++ models/user-model.js
@@ -5,8 +5,6 @@ const UserSchema = sequelize.define("UserSchema",{
     id: {type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true},
     email: {type: DataTypes.STRING, allowNull: false, unique: true},
     password: {type: DataTypes.STRING, allowNull: false},
-    isActivated: {type: DataTypes.BOOLEAN, defaultValue: false},
-    activationLink: {type: DataTypes.STRING},
 })
 
 export default UserSchema
